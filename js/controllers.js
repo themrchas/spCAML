2ce80ada2cb39946631bbc0732b3c9045d6d97ac
Charles Geib <charles.r.geib.ctr@mail.mil>
2019-04-01 12:26:52 +0000

    mod caml query

--- js/controllers.js
+++ js/controllers.js
@@ -9,18 +9,22 @@ var app = angular.module("app1",[]);
 
     $scope.test = "helloWorld";
 
-
-    //var targetUrl = "http://localhost:8080/sites/dev/socafdev/_api/Web/Lists/getByTitle('CountryLinks')/items?$select=URL";
-    var targetUrl = "http://localhost:8080/sites/dev/socafdev/_api/Web/Lists/getByTitle('CountryLinks')/GetItems(query=@v1)?";
-   // var camlQuery =  "<View><Query><Where><Eq><FieldRef Name='Country'/><Value Type='TaxonomyFieldType'>{{countryMM}}</Value></Eq></Where></Query></View>";
-   var requestBodyJSON = { "query" : { "__metadata": { "type" : "SP.CamlQuery" } , "ViewXml": camlQuery }};
-  //  requestBodyString = "@v1=" + JSON.stringify(requestBodyJSON );
-
-   //var camlQuery =  { ViewXml: "<View><Query><Where><Eq><FieldRef Name='CountryMM'/><Value Type='TaxonomyFieldType'>Germany</Value></Eq></Where></Query></View>" };                 
+    //
+    var targetUrl = "http://localhost:8080/sites/dev/socafdev/_api/Web/Lists/getByTitle('CountryPagesContacts')/GetItems(query=@v1)?";
+   
+    var requestBodyJSON = { "query" : { "__metadata": { "type" : "SP.CamlQuery" } , "ViewXml": camlQuery }};
+  
     
-   var camlQuery =  { ViewXml: "<View><Query><Where><In><FieldRef Name='testMMM' LookupId='TRUE' /><Values><Value Type='Integer'>4</Value></Values></In></Where></Query></View>" };                 
-    
+ /* Working Country Links query using multi choice managed meta-data
+  var targetUrl = "http://localhost:8080/sites/dev/socafdev/_api/Web/Lists/getByTitle('CountryLinks')/GetItems(query=@v1)?";
+  var camlQuery =  { ViewXml: "<View><Query><Where><Eq><FieldRef Name='testMMM'/><Value Type='Text'>Germany</Value></Eq></Where></Query></View>" };  
+*/
 
+//var camlQuery =  { ViewXml: "<View><Query><Where><Eq><FieldRef Name='Countries'/><Value Type='Text'>Germany</Value></Eq></Where></Query></View>" };  
+var camlQuery =  { ViewXml: "<View><Query><OrderBy><FieldRef Name='SOFCategory' Ascending='TRUE'/></OrderBy><Where><Eq><FieldRef Name='Countries'/><Value Type='Text'>Germany</Value></Eq></Where></Query></View>" };  
+
+
+    
   // var camlQuery =  { ViewXml: "<View><Query><Where><In><FieldRef LookupId='True' Name='testMMM'/><Values><Value Type='LookupMulti'>Germany</Value><Value Type='LookupMulti'>Ghana</Value></Values></In></Where></Query></View>" };                 
      
  //var camlQuery =  { ViewXml: "<View><Query><Where><Contains><FieldRef LookupId='True' Name='testMMM'/><Value Type='LookupMulti'>Germany</Value></Contains></Where></Query></View>" };                 
